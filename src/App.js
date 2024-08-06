a103f7b27f0ff289359f0f723a01e0aa7c5a1f4f
Arpit Mohankar <arpitmohankar28@gmail.com>
2024-08-06 21:49:45 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ export default function App() {
       setListening(false);
     };
 
+    
     const speakText = (text) => {
     const synth = window.speechSynthesis;
     const voices = synth.getVoices();
